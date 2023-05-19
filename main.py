91a2d55e0bea86988afec4307a6cbe0c0a637926
vis256 <mateusz10077@gmail.com>
2023-05-19 12:27:36 +0200

    Add colors

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from mpi4py import MPI
 
+class TERMCOLORS:
+    BLUE = '\033[94m',
+    CYAN = '\033[96m',
+    GREEN = '\033[92m',
+    WARNING = '\033[93m',
+
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
