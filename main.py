1be9c13e91f09aea95897e134250c15dca5d0a0c
nebulazc <67900555+vis256@users.noreply.github.com>
2023-05-24 13:00:23 +0200

    Setup functions

--- main.py
+++ main.py
@@ -1,48 +1,100 @@
 from mpi4py import MPI
-from enum import Enum
+from enum import IntEnum
+import time
 
 class TERMCOLORS:
-    BLUE = '\033[94m',
-    CYAN = '\033[96m',
-    GREEN = '\033[92m',
-    WARNING = '\033[93m',
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
 
+COLORS = {0: TERMCOLORS.BLUE, 1: TERMCOLORS.CYAN, 2: TERMCOLORS.GREEN, 3: TERMCOLORS.WARNING}
 
-STATES = Enum("STATES", ["REST", "WAIT", "INSECTION", "PAUSE", "REPLACING"])
+class STATES(IntEnum):
+    REST = 1
+    WAIT = 2
+    INSECTION = 3
+    PAUSE = 4
+    REPLACING = 5
 
-clock = 0
+class TAGS(IntEnum):
+    REQ = 1
+    ACK = 2
+    RELEASE = 3
+    EMPTY = 4
+    RESUME = 5
+
+clock : int = 0
+
+class Message:
+    def __init__(self, tag : TAGS, data : str):
+        global clock
+        self.tag : int = tag
+        self.data : str = data
+        self.clock : int = clock
+
+CURRENT_STATE = STATES.PAUSE
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+status = MPI.Status()
 
-def embedClockWithMsg(data):
-    global clock
-    return {
-        "clock": clock,
-        "msg": data
-    }
-
-def send(tag, dest, msg=""):
+def send(tag, dest, data=""):
     global clock, comm
-    comm.send(msg, dest, tag)
+    comm.send(Message(tag, data), dest)
     clock += 1
 
-def multisend(tag, dest, msg=""):
+def multisend(tag, dest, data=""):
     global clock, comm
     for d in dest:
-        comm.send(msg, d, tag)
+        comm.send(Message(tag, data), d)
     clock += 1
+
+def broadcast(tag, data=""):
+    global clock, comm
+    for i in range(comm.Get_size()):
+        if i != rank:
+            comm.send(Message(tag, data), i)
+    clock += 1
+
+def receive() -> Message:
+    global clock, status
+    msg : Message = comm.recv()
+    clock += 1
+    return msg
 
 def debug(msg):
     global clock, rank
-    print(f"{TERMCOLORS.BLUE}[{rank}][{clock}] {msg}")
+    print(f"{COLORS[rank % 3]}[{rank}][{clock}] {msg}")
 
-while True:
-    debug("Waiting for recv")
-    if not rank == 0:
-        received = comm.recv()
+def onReceivePause(msg : Message):
+    debug(f"Received: tag={msg.tag} msg={msg.data}")
 
-        debug(f"Received={received}")
 
-    if rank == 0:
-        send("ACK", 1, "asdfrt")
+def main():
+    while True:
+        debug("Waiting for recv")
+        data = receive()
+
+        if CURRENT_STATE == STATES.REST:
+            pass
+        elif CURRENT_STATE == STATES.WAIT:
+            pass
+        elif CURRENT_STATE == STATES.INSECTION:
+            pass
+        elif CURRENT_STATE == STATES.PAUSE:
+            onReceivePause(data)
+        elif CURRENT_STATE == STATES.REPLACING:
+            pass
+        else:
+            debug("Invalid state")
+
+        time.sleep(2)
+
+    
+        # send("ACK", 1, "asdfrt")
+
+if rank == 0:
+    broadcast(TAGS.ACK, "Test msg")
+else:
+    main()
