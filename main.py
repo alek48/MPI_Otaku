dc04593fe1c07c17c904743958c1a1bdb7d11242
nebulazc <67900555+vis256@users.noreply.github.com>
2023-05-24 13:17:35 +0200

    More

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import List
 from mpi4py import MPI
 from enum import IntEnum
 import time
@@ -18,26 +19,59 @@ class STATES(IntEnum):
     REPLACING = 5
 
 class TAGS(IntEnum):
-    REQ = 1
-    ACK = 2
+    ACK = 1
+    REQ = 2
     RELEASE = 3
     EMPTY = 4
     RESUME = 5
 
 clock : int = 0
 
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
 class Message:
     def __init__(self, tag : TAGS, data : str):
-        global clock
+        global clock, rank
         self.tag : int = tag
         self.data : str = data
         self.clock : int = clock
+        self.sender : int = rank
 
+    def __str__(self):
+        return f"Message=(tag={self.tag}, data={self.data} clock={self.clock} sender={self.sender})"
+
+class QueueMember:
+    def __init__(self, rank : int, clock : int, gas : int):
+        self.rank = rank
+        self.clock = clock
+        self.gas = gas
+
+    def __str__(self):
+        return f"Member=(rank={self.rank} clock={self.clock} gas={self.gas})"
+
+PREVIOUS_STATE = STATES.REST
 CURRENT_STATE = STATES.PAUSE
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-status = MPI.Status()
+def changeState(newState):
+    global PREVIOUS_STATE, CURRENT_STATE
+    PREVIOUS_STATE = CURRENT_STATE
+    CURRENT_STATE = newState
+
+messageFreezer : List[Message] = []
+RoomGas : int = 0
+InhaledGas : int = 0
+WaitQueue : List[QueueMember] = []
+LastResume : int = 0
+
+def addToQueue(rank, clock, gas):
+    global WaitQueue
+    WaitQueue.append( QueueMember(rank, clock, gas) )
+    WaitQueue = sorted(WaitQueue, key=lambda x : x.gas)
+
+def removeFromQueue(rank):
+    global WaitQueue
+    WaitQueue = [x for x in WaitQueue if x.rank != rank]
 
 def send(tag, dest, data=""):
     global clock, comm
@@ -67,8 +101,42 @@ def debug(msg):
     global clock, rank
     print(f"{COLORS[rank % 3]}[{rank}][{clock}] {msg}")
 
+
+
 def onReceivePause(msg : Message):
-    debug(f"Received: tag={msg.tag} msg={msg.data}")
+    global RoomGas, InhaledGas, LastResume
+
+    debug(f"Received: {msg}")
+    
+    # REQ - Przechowuje wiadomość, żeby obsłużyć ją po opuszczeniu tego stanu (opisane poniżej).
+    if (msg.tag == TAGS.REQ):
+        messageFreezer.append(msg)
+
+    # ACK - Przechowuje wiadomość, żeby obsłużyć ją po opuszczeniu tego stanu.
+    elif (msg.tag == TAGS.ACK):
+        messageFreezer.append(msg)
+
+    # RELEASE - Usuwa nadawcę z WaitQueue, aktualizuje RoomGas oraz InhaledGas (opisane poniżej).
+    elif (msg.tag == TAGS.RELEASE):
+        removeFromQueue(msg.sender)
+        # TODO: update gas
+
+    # EMPTY - Ignoruje; sytuacja niemożliwa.
+    elif (msg.tag == TAGS.EMPTY):
+        pass
+
+    # RESUME - Wraca do stanu poprzedniego (lub REST, jeśli wcześniej był INSECTION), ustawia LastResume na zegar Lamporta tej wiadomości i ustawia InhaledGas na 0.
+    elif (msg.tag == TAGS.RESUME):
+        if PREVIOUS_STATE == STATES.INSECTION:
+            changeState(STATES.REST)
+        else:
+            changeState(PREVIOUS_STATE)
+
+        LastResume = msg.clock
+
+        InhaledGas = 0
+
+
 
 
 def main():
@@ -90,9 +158,6 @@ def main():
             debug("Invalid state")
 
         time.sleep(2)
-
-    
-        # send("ACK", 1, "asdfrt")
 
 if rank == 0:
     broadcast(TAGS.ACK, "Test msg")
